39ab0e182941aa11c94e818e69006570562f474b
robin <robenjay@hotmail.nl>
2023-07-08 23:43:59 +0200

    added comments to the unit tests

--- src/test/java/com/quintor/parser/service/JsonServiceTest.java
+++ src/test/java/com/quintor/parser/service/JsonServiceTest.java
@@ -20,15 +20,23 @@ class JsonServiceTest {
         jsonFile = new JsonService();
     }
 
+    /**
+     * making a unit test for the json parser and then the equals version
+     * @throws Exception
+     */
     @Test
     void parseFileJsonEquals() throws Exception {
 
+        //grab the file
         File jsonTest1File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test.940");
 
+        //make a bufferedInputStream, so it doesn't do it all at once and make a fileInputStream inside it
         BufferedInputStream jsonBs1 = new BufferedInputStream(new FileInputStream(jsonTest1File));
 
+        //small byte array so all the bytes of the file can be sorted
         byte[] jsonFileContent = jsonBs1.readAllBytes();
 
+        //make a multiPartFile mock, so you don't need an actual live file
         MockMultipartFile jsonFileTest = new MockMultipartFile("file", "test.940", "text/plain", jsonFileContent);
 
         String jsonResult = jsonFile.parseFile(jsonFileTest);
@@ -36,21 +44,30 @@ class JsonServiceTest {
         assertEquals(jsonResult, jsonResult, "the json file isn't the same as the other file or something is broken in JsonService");
     }
 
+    /**
+     * making a unit test for the json parser and then the not equals version
+     * @throws Exception
+     */
+
     @Test
     void parseFileJsonNotEquals() throws Exception {
 
+        //grab the file
         File jsonTest1File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test.940");
 
         File jsonTest2File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test3.940");
 
+        //make a bufferedInputStream, so it doesn't do it all at once and make a fileInputStream inside it
         BufferedInputStream jsonBs1 = new BufferedInputStream(new FileInputStream(jsonTest1File));
 
         BufferedInputStream jsonBs2 = new BufferedInputStream(new FileInputStream(jsonTest2File));
 
+        //small byte array so all the bytes of the file can be sorted
         byte[] jsonFileContent = jsonBs1.readAllBytes();
 
         byte[] jsonFileContent2 = jsonBs2.readAllBytes();
 
+        //make a multiPartFile mock, so you don't need an actual live file
         MockMultipartFile jsonFileTest1 = new MockMultipartFile("file", "test.940", "text/plain", jsonFileContent);
 
         MockMultipartFile jsonFileTest2 = new MockMultipartFile("file", "test3.940", "text/plain", jsonFileContent2);
--- src/test/java/com/quintor/parser/service/XmlServiceTest.java
+++ src/test/java/com/quintor/parser/service/XmlServiceTest.java
@@ -17,15 +17,23 @@ class XmlServiceTest {
         xmlFile = new XmlService();
     }
 
+    /**
+     * making a unit test for mt940 file parsing to xml the equals version
+     * @throws IOException
+     */
     @Test
     void parseFileXmlEquals() throws IOException {
 
+        //grab the file
         File xmlTest1File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test.940");
 
+        //make a buffered input stream, so it doesn't load all at once and make a file input stream inside
         BufferedInputStream xmlBs1 = new BufferedInputStream(new FileInputStream(xmlTest1File));
 
+        //make a byte array so all the bytes can be read from the file
         byte[] xmlFileContent = xmlBs1.readAllBytes();
 
+        //make a mock from the Multi part file so you don't need a live file
         MockMultipartFile xmlFileTest = new MockMultipartFile("file", "test.940", "text/plain", xmlFileContent);
 
         String xmlResult = xmlFile.parseFile(xmlFileTest);
@@ -34,21 +42,30 @@ class XmlServiceTest {
 
     }
 
+
+    /**
+     * making a unit test for mt940 file parsing to xml the not equals version
+     * @throws Exception
+     */
     @Test
     void parseFileXmlNotEquals() throws Exception {
 
+        //grab the file
         File xmlTest1File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test.940");
 
         File xmlTest2File = new File("src\\test\\java\\com\\quintor\\parser\\files\\test3.940");
 
+        //make a buffered input stream, so it doesn't load all at once and make a file input stream inside
         BufferedInputStream xmlBs1 = new BufferedInputStream(new FileInputStream(xmlTest1File));
 
         BufferedInputStream xmlBs2 = new BufferedInputStream(new FileInputStream(xmlTest2File));
 
+        //make a byte array so all the bytes can be read from the file
         byte[] xmlFileContent = xmlBs1.readAllBytes();
 
         byte[] xmlFileContent2 = xmlBs2.readAllBytes();
 
+        //make a mock from the Multi part file so you don't need a live file
         MockMultipartFile xmlFileTest1 = new MockMultipartFile("file", "test.940", "text/plain", xmlFileContent);
 
         MockMultipartFile xmlFileTest2 = new MockMultipartFile("file", "test3.940", "text/plain", xmlFileContent2);
